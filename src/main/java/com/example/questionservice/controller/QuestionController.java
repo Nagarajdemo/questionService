40adae107559ff2063b8b00ba4333ee86fd028a7
unknown <nagaraja1978@gmail.com>
2025-01-05 23:18:26 +0530

    added response entity

--- src/main/java/com/example/questionservice/controller/QuestionController.java
+++ src/main/java/com/example/questionservice/controller/QuestionController.java
@@ -3,7 +3,9 @@ package com.example.questionservice.controller;
 import com.example.questionservice.model.Question;
 import com.example.questionservice.service.QuestionService;
 import jakarta.websocket.server.PathParam;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,17 +17,17 @@ public class QuestionController {
  QuestionService questionService;
 
     @GetMapping("allQuestions")
-    public List<Question> getAllQuestions(){
+    public ResponseEntity<List<Question>> getAllQuestions(){
         return  questionService.getAllQuestions();
     }
 
     @GetMapping("category/{category}")
-    public List<Question>getByCatergory(@PathVariable String category){
+    public ResponseEntity<List<Question>>getByCatergory(@PathVariable String category){
         return questionService.getByCategory(category);
     }
 
     @PostMapping("add")
-    public String addQuestions(@RequestBody Question question){
+    public ResponseEntity<String> addQuestions(@RequestBody Question question){
         return questionService.addQuestion(question);
 
     }
--- src/main/java/com/example/questionservice/service/QuestionService.java
+++ src/main/java/com/example/questionservice/service/QuestionService.java
@@ -3,24 +3,41 @@ package com.example.questionservice.service;
 import com.example.questionservice.dao.QuestionDAO;
 import com.example.questionservice.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class QuestionService {
     @Autowired
     QuestionDAO questionDAO;
-    public List<Question> getAllQuestions() {
-        return questionDAO.findAll();
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try {
+            return new ResponseEntity<>(questionDAO.findAll(),HttpStatus.OK);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
     }
-    public String addQuestion(Question question) {
-
-        questionDAO.save(question);
-        return "successfully saved";
+    public ResponseEntity<String> addQuestion(Question question) {
+        try {
+            questionDAO.save(question);
+            return new ResponseEntity<>("successfully saved",HttpStatus.CREATED);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>("failure",HttpStatus.BAD_REQUEST);
     }
 
-    public List<Question> getByCategory(String category) {
-        return questionDAO.getQuestionByCategory(category);
+    public ResponseEntity<List<Question>> getByCategory(String category) {
+        try {
+            return new ResponseEntity<>(questionDAO.getQuestionByCategory(category), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
     }
 }
